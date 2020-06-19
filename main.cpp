5370039534760cf1f2c9de2ab03945d94d76a191
Jorge Luis Emmanuel Espinoza Espinoza <35707910+yoshi70001@users.noreply.github.com>
2020-06-19 02:10:07 -0500

    añadieron botones

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "Circulo.h"
  using namespace std;
  Punto absd[20];
+ int opcion;
 void coordenadas(){
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1,1,0);// color
@@ -17,6 +18,44 @@ void coordenadas(){
 	glVertex2f (0, 400);
 	glVertex2f (800, 400);
 	glEnd();	
+}
+void botones(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1,1,0.5);// color
+  	glBegin (GL_POLYGON);
+  	glVertex2f (0,20);
+	glVertex2f (20, 20);
+	glVertex2f (20, 0);
+	glVertex2f (0,0);
+	glEnd();
+	glColor3f(1,1,0);// color
+  	glBegin (GL_POLYGON);
+  	glVertex2f (0,20+30);
+	glVertex2f (20, 20+30);
+	glVertex2f (20, 0+30);
+	glVertex2f (0,0+30);
+	glEnd();
+	glColor3f(1,0.5,0);// color
+	glBegin (GL_POLYGON);
+  	glVertex2f (0,20+60);
+	glVertex2f (20, 20+60);
+	glVertex2f (20, 0+60);
+	glVertex2f (0,0+60);
+	glEnd();
+	glColor3f(1,0,0);// color
+	glBegin (GL_POLYGON);
+  	glVertex2f (0,20+90);
+	glVertex2f (20, 20+90);
+	glVertex2f (20, 0+90);
+	glVertex2f (0,0+90);
+	glEnd();
+	glColor3f(0.5,0,0);// color
+	glBegin (GL_POLYGON);
+  	glVertex2f (0,20+120);
+	glVertex2f (20, 20+120);
+	glVertex2f (20, 0+120);
+	glVertex2f (0,0+120);
+	glEnd();
 }
 
 	float Y;
@@ -28,15 +67,33 @@ void mi_Mouse(int button, int state, int x, int y) {
       glColor3f(1,1,0);
     absd[NUMPOINTS].setx((float)(800 -x));
     absd[NUMPOINTS].sety((float)y);
+    int n1=absd[NUMPOINTS].getx();
+	int n2=absd[NUMPOINTS].gety();
+    if(n1<=20 && n1>>0 && n2<=20 && n2>=0){
+    	opcion=1;
+    	NUMPOINTS = 0;
+	}
+	if(n1<=20 && n1>>0 && n2<=50 && n2>=30){
+		opcion=2;
+		NUMPOINTS = 0;
+	}
+	if(n1<=20 && n1>>0 && n2<=80 && n2>=60){
+		opcion=3;
+		NUMPOINTS = 0;
+	}
+	if(n1<=20 && n1>>0 && n2<=110 && n2>=90){
+		opcion=4;
+		NUMPOINTS = 0;
+	}
     
     NUMPOINTS++;
     
   }
-}int opcion;
+}
 void dibujar(){
 	glClear(GL_COLOR_BUFFER_BIT);
-	coordenadas();
-	
+	botones();
+		absd[NUMPOINTS+1].draw();
 	
 	if(NUMPOINTS == 2 && opcion==1) {
         glColor3f(1.0,1.0,0.0);
@@ -45,11 +102,23 @@ void dibujar(){
         NUMPOINTS = 0;
     }
     if(NUMPOINTS == 5 && opcion==2) {
-        glColor3f(1.0,1.0,0.0);
+        glColor3f(1.0,0.5,0.0);
         Curva uwd(absd,5);
            uwd.bezier();
         NUMPOINTS = 0;
     }
+    if(NUMPOINTS == 4 && opcion == 3){
+    	Poligonos ads(absd,4);
+    	ads.dibujarpoligono();
+    	NUMPOINTS = 0;
+	}
+	if(NUMPOINTS == 2 && opcion == 4){
+		float distancia=sqrt(abs(pow(absd[0].getx()-absd[1].getx(),2)-pow(absd[0].gety()-absd[1].gety(),2)));
+		cout<<distancia;
+    	Circulo ads(absd[0].getx(),absd[0].gety(),distancia,200);
+    	ads.dibujarcirculo();
+    	NUMPOINTS = 0;
+	}
     
     glFlush();
 }
@@ -64,9 +133,7 @@ void iniciar(){
 
 
 int main(int argc, char** argv) {
-	cout<<"linea =1 bezier=2";
-	cin>>opcion;
-	if(opcion==1||opcion==2){
+	
 		glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE| GLUT_RGB);
 	glutInitWindowPosition(50,50);
@@ -76,11 +143,7 @@ int main(int argc, char** argv) {
 	glutDisplayFunc(dibujar);
 	
 		iniciar();
-		
-	glutMainLoop();
-	}
-		
-	
+		glutMainLoop();
 	
 	
 }
